1fb24722dc9f8f99606cb18e5a8150a4a833ea32
jdickey <32DAZpzRa9@s>
2018-05-29 15:27:21 -0600

    Added UserOptions table and function to load that table with data.

--- databaseLoader.py
+++ databaseLoader.py
@@ -6,6 +6,9 @@ import fnmatch
 
 
 # this is needed due to some values being too laarge for default size
+from typing import Any, Union
+
+
 def setMaxFiledLimit():
     maxInt = sys.maxsize
     decrement = True
@@ -24,19 +27,18 @@ def setMaxFiledLimit():
 
 # takes a table name and a list of parameters
 # Will drop the table if it exists
-def createTable(table_name, headers):
+def createTable(cursor, table_name, headers):
     cursor.execute('''drop table if exists ''' + table_name)
 
     cursor.execute('''CREATE TABLE ''' + table_name + '''(''' + ','.join(headers) + ''')''')
 
-
-def createRequestUserOptionsJoinTable(table_name):
-    cursor.execute('''drop table if exists ''' + table_name)
-    column_names = 'RowId TEXT PRIMARY KEY,  RequestRowId, RequestUserOptionsRowId, ' \
-                   'FOREIGN KEY (RequestRowId) REFERENCES Request(RowId) ' \
-                   'FOREIGN KEY (RequestUserOptionsRowId) REFERENCES (RequestUserOptions)'
-    cursor.execute('''CREATE TABLE ''' + table_name + '''(''' + column_names + ''')''')
-    print('User Options Junction table made')
+# def createRequestUserOptionsJoinTable(cursor, table_name):
+#     cursor.execute('''drop table if exists ''' + table_name)
+#     column_names = 'RowId TEXT PRIMARY KEY,  RequestRowId, RequestUserOptionsRowId, ' \
+#                    'FOREIGN KEY (RequestRowId) REFERENCES Request(RowId) ' \
+#                    'FOREIGN KEY (RequestUserOptionsRowId) REFERENCES (RequestUserOptions)'
+#     cursor.execute('''CREATE TABLE ''' + table_name + '''(''' + column_names + ''')''')
+#     print('User Options Junction table made')
 
 
 def createUserOptionsHeader():
@@ -53,11 +55,28 @@ def createUserOptionsHeader():
                 counter += 1
                 if counter == 1:
                     for item in row:
-                        if item not in header:
-                            header.append(item)
+                        if item.lower() not in header:
+                            header.append(item.lower())
 
     print('User Options column headers  made')
     return header
+
+def readUserOptionCsv(cursor, col_names):
+    path = "C:\\Users\\jdickey\\Desktop\\SN imports\\"
+    pattern = 'Request_UserOption*'
+
+    for root, dirs, files in os.walk(path):
+        for file in fnmatch.filter(files, pattern):
+            reader = csv.reader(open(root + '\\' + file, 'r', encoding="utf8"), delimiter=',')
+            counter = 0
+            for row in reader:
+                counter += 1
+                if counter == 1:
+                    header = alterheaders(row)
+                    continue
+
+                cursor.execute('''INSERT INTO UserOptions ({0}) VALUES ({1})'''.format(','.join(header),
+                                                                                       ','.join(['?'] * len(row))), row)
 
 
 # takes a foldername and a table name as parameters
@@ -110,32 +129,15 @@ def getheaders(folder_name):
                 break
 
 
-setMaxFiledLimit()
+def alterheaders(colnames):
+    # headers = ['SAWRowId_c' if x == 'RowId' else x for x in colnames]
+    # Alters any header named 'Index' as that is a reserved keyword
+    headers = ['Index_c' if x == 'Index' else x for x in colnames]
+    headers = ['Index_c' if x == 'index' else x for x in headers]
 
-conn = sqlite3.connect('SNImport.db')
-cursor = conn.cursor()
+    return headers
 
-# lists of table names and foreign keys as needed
-table_names = ['Person', 'PersonGroup', 'ServiceDefinition', 'Category', 'Article', 'Change', 'Incident', 'Request',
-               'UserOptions']
-person_foreign_keys = ['OwnedByPerson', 'AssignedPerson', 'RecordedByPerson', 'RequestedByPerson', 'LastUpdateByPerson',
-                       'CreatedByPerson', 'ClosedByPerson', 'ContactPerson', 'SolvedByPerson', 'CompOpsExpertOnCall_c',
-                       'CompOpsOnCall_c']
-person_group_foreign_keys = ['KMAssignedGroup_c', 'OwnedByGroup' 'CompOpsGroup_c', 'AssignedGroup']
-service_foreign_keys = ['Service']
-
-# iterates the list of tablenames, creates and loads the tables with data
-for item in table_names:
-    if item == 'UserOptions':
-        header = createUserOptionsHeader()
-        header = ['RowId PRIMARY KEY' if x == 'RowId' else x for x in header]
-        header = ['Index_c' if x == 'Index' else x for x in header]
-
-    else:
-        header = getheaders(item)
-        # sets the 'Id' column as the PRIMARY KEY
-        header = ['Id INTEGER PRIMARY KEY' if x == 'Id' else x for x in header]
-
+def setforeignkeys(header):
     # adds the FOREIGN KEY constrainds for any columns in the related list
     for key in person_foreign_keys:
         if key in header:
@@ -150,11 +152,53 @@ for item in table_names:
         if key in header:
             header.append('FOREIGN KEY (' + key + ') REFERENCES PersonGroup(Id)')
 
-    createTable(item, header)
-    readAllcsv(item, item)
-    #TODO load in userOptions data from each user optoin file.
-    print('Table: ' + item + ' had bean created and loaded with data')
+    for key in user_options_foreign_keys:
+        if key in header:
+            header.append('FOREIGN KEY (' + key + ') REFERENCES Request(RowId)')
 
+    for key in offering_foreign_keys:
+        if key in header:
+            if key in header:
+                header.append('FOREIGN KEY (' + key + ') REFERENCES Offering(Id)')
+    return header
+
+
+setMaxFiledLimit()
+
+conn = sqlite3.connect('SNImport.db')
+cursor = conn.cursor()
+
+# lists of table names and foreign keys as
+table_names = ['Person', 'PersonGroup', 'ServiceDefinition', 'Category', 'Article', 'Change',# 'Incident', 'Request',
+               'UserOptions', 'Offering']
+person_foreign_keys = ['OwnedByPerson', 'AssignedPerson', 'RecordedByPerson', 'RequestedByPerson', 'LastUpdateByPerson',
+                       'CreatedByPerson', 'ClosedByPerson', 'ContactPerson', 'SolvedByPerson', 'CompOpsExpertOnCall_c',
+                       'CompOpsOnCall_c']
+person_group_foreign_keys = ['KMAssignedGroup_c', 'OwnedByGroup' 'CompOpsGroup_c', 'AssignedGroup']
+service_foreign_keys = ['Service']
+user_options_foreign_keys = ['parentrowid']
+offering_foreign_keys = ['RequestsOffering']
+
+# iterates the list of tablenames, creates and loads the tables with data
+for item in table_names:
+    if item == 'UserOptions':
+        header = createUserOptionsHeader()
+        header = alterheaders(header)
+    else:
+        header = getheaders(item)
+        # sets the 'Id' column as the PRIMARY KEY
+        header = ['Id INTEGER PRIMARY KEY' if x == 'Id' else x for x in header]
+
+    header = setforeignkeys(header)
+
+    createTable(cursor, item, header)
+    conn.commit()
+    readAllcsv(item, item)
+    conn.commit()
+    if item == 'UserOptions':
+        readUserOptionCsv(cursor, header)
+    
+    print('Table: ' + item + ' had bean created and loaded with data')
 
 conn.commit()
 
